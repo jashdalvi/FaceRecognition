ccc736663ac6265172c8c17335c2d986bf98c563
jashdalvi <jash.dalvi@somaiya.edu>
2021-02-27 20:35:27 +0530

    Improved Face Recognition model

--- src/temp.py
+++ src/temp.py
@@ -1,23 +1,36 @@
-import cv2
-import os
-import imutils
-from tensorflow.keras.preprocessing.image import img_to_array,load_img,ImageDataGenerator
+import pickle
+from sklearn.model_selection import cross_val_score,StratifiedKFold,GridSearchCV
+from sklearn.preprocessing import LabelEncoder,StandardScaler
+from sklearn.svm import LinearSVC,SVC
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
 import numpy as np
+import os
 
-beard_path = os.path.join(os.path.join("..",'data_aug'),'beard1.png')
 
-aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
-                        height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-                        horizontal_flip=True, fill_mode="nearest")
 
-image = load_img(beard_path)
-image = img_to_array(image)
-image = np.expand_dims(image,axis = 0)
-label = "beard"
-save_to_dir = "temp"
-imageGen = aug.flow(image, batch_size=1, save_to_dir=save_to_dir,save_prefix=label, save_format="jpg")
-total = 0 
-for image in imageGen:
-    total +=1
+emb_path = os.path.join(os.path.join("..",'data'),"data.pickle")
+with open(emb_path,'rb') as f:
+    data = pickle.load(f)
 
-print("Produced {} images in total".format(str(total)))
+X = data['embeddings']
+y = data['labels']
+
+print('Creating data for ML model')
+np.random.seed(42)
+scalar = StandardScaler()
+X = np.array(X)
+y = np.array(y)
+indexes = np.random.permutation(np.arange(X.shape[0]))
+le = LabelEncoder()
+y = le.fit_transform(y)
+X = X[indexes]
+y = y[indexes]
+#model = LinearSVC()
+model = SVC()
+pipeline = Pipeline([('transformer', scalar), ('estimator',model)])
+cv = StratifiedKFold(n_splits=5)
+param_grid = {'estimator__C':[0.0001,0.001,0.01,0.1,1,10],'estimator__kernel':['linear','rbf'],'estimator__gamma':['auto','scale']}
+grid_svc = GridSearchCV(pipeline,param_grid = param_grid,scoring = 'accuracy',verbose = True,cv = cv,n_jobs = -1)
+grid_svc.fit(X,y)
+print("The Best score for svc model is {} with params {}".format(grid_svc.best_score_,grid_svc.best_params_))
--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.model_selection import cross_val_score,StratifiedKFold,GridSearchCV
 from sklearn.preprocessing import LabelEncoder,StandardScaler
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 import numpy as np
@@ -13,30 +13,30 @@ emb_path = os.path.join(os.path.join("..",'data'),"data.pickle")
 with open(emb_path,'rb') as f:
     data = pickle.load(f)
 
-X = data['embeddings']
-y = data['labels']
+#X = data['embeddings']
+#y = data['labels']
 
 print('Creating data for ML model')
-np.random.seed(42)
-scalar = StandardScaler()
-X = np.array(X)
-y = np.array(y)
-indexes = np.random.permutation(np.arange(X.shape[0]))
-le = LabelEncoder()
-y = le.fit_transform(y)
-X = X[indexes]
-y = y[indexes]
-model = LinearSVC()
+#np.random.seed(42)
+#scalar = StandardScaler()
+#X = np.array(X)
+#y = np.array(y)
+#indexes = np.random.permutation(np.arange(X.shape[0]))
+#le = LabelEncoder()
+#y = le.fit_transform(y)
+#X = X[indexes]
+#y = y[indexes]
+#model = LinearSVC()
 #model = SVC(kernel = 'linear')
-pipeline = Pipeline([('transformer', scalar), ('estimator',model)])
+#pipeline = Pipeline([('transformer', scalar), ('estimator',model)])
 #pipeline = Pipeline([('estimator',model)])
-cv = StratifiedKFold(n_splits=5)
-grid_svc = GridSearchCV(pipeline,param_grid = {'estimator__C':[0.0001,0.001,0.01,0.1,1,10]},scoring = 'accuracy',verbose = True,cv = cv,n_jobs = -1)
-grid_svc.fit(X,y)
-print("The Best score for svc model is {} with params {}".format(grid_svc.best_score_,grid_svc.best_params_))
-cross_val_scores = cross_val_score(grid_svc,X,y,cv = cv)
+#cv = StratifiedKFold(n_splits=5)
+#param_grid = {'estimator__C':[0.0001,0.001,0.01,0.1,1,10]}
+#grid_svc = GridSearchCV(pipeline,param_grid = param_grid,scoring = 'accuracy',verbose = True,cv = cv,n_jobs = -1)
+#grid_svc.fit(X,y)
+#print("The Best score for svc model is {} with params {}".format(grid_svc.best_score_,grid_svc.best_params_))
 
-print('The cross-validation  score for Linear Support vector Machine model is {}'.format(cross_val_scores.mean()))
+#print('The cross-validation  score for Linear Support vector Machine model is {}'.format(cross_val_scores.mean()))
 
 print('Now training on whole data and saving model')
 #scalar_all = StandardScaler()
@@ -56,7 +56,7 @@ y = y[indexes]
 X = scalar_all.fit_transform(X)
 print(X.shape)
 print(y.shape)
-model = grid_svc.best_estimator_
+model = SVC(kernel = 'rbf',C = 10,gamma = 'auto')
 model.fit(X,y)
 y_pred = model.predict(X)
 print(classification_report(y,y_pred,target_names = le.classes_))
